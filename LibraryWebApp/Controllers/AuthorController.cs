c773f4d5d52263254cac3e05598116dd3f3f173a
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-03 11:15:28 +0100

    Making view for displaying authors

---
+++ LibraryWebApp/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using LibraryWebApp.Data;
+using LibraryWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryWebApp.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public AuthorController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Author> objList = _db.Author;
+            return View(objList);
+        }
+    }
+}
