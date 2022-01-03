00bf71cf61529c61f5a0bd679b4f8c636cbe6fde
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-03 12:21:36 +0100

    Add Edit View to edit Author details

--- LibraryWebApp/Controllers/AuthorController.cs
+++ LibraryWebApp/Controllers/AuthorController.cs
@@ -42,6 +42,34 @@ namespace LibraryWebApp.Controllers
             return View(obj);
         }
 
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Author.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Author obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Author.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
