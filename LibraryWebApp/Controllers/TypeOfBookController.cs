149a1d86cc8e0fc2806e8050aad1a33e57e7c78e
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-03 10:05:48 +0100

    Add CRUD to Type of Book

--- LibraryWebApp/Controllers/TypeOfBookController.cs
+++ LibraryWebApp/Controllers/TypeOfBookController.cs
@@ -42,5 +42,49 @@ namespace LibraryWebApp.Controllers
             }
             return View(obj);
         }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.TypeOfBook.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(TypeOfBook obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TypeOfBook.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.TypeOfBook.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.TypeOfBook.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
