356574ccf079cd52d1f54b1c7dafed0253b50b12
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-03 11:04:39 +0100

    Adding Migration

--- LibraryWebApp/Migrations/20220103100236_AddAuthorToDatabase.Designer.cs
+++ LibraryWebApp/Migrations/20220103100236_AddAuthorToDatabase.Designer.cs
@@ -3,14 +3,16 @@ using LibraryWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LibraryWebApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220103100236_AddAuthorToDatabase")]
+    partial class AddAuthorToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
