554d831898889d4ef2e71e7497a540351fd86585
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-05 08:59:51 +0100

    Add Book and BookAuthor Model

---
+++ LibraryWebApp/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryWebApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public int Amount { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
