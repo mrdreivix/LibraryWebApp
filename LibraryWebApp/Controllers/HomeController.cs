a9216f811af7e7cf83bf7d40e66a38456c5a8c48
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-14 21:14:22 +0100

    Add Main Page Functionality

--- LibraryWebApp/Controllers/HomeController.cs
+++ LibraryWebApp/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using LibraryWebApp.Models;
+﻿using LibraryWebApp.Data;
+using LibraryWebApp.Models;
+using LibraryWebApp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +15,26 @@ namespace LibraryWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDbContext _db;
+        public HomeController(ILogger<HomeController> logger, AppDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Book> books = _db.Book
+                .Include(x => x.BookAuthor)
+                .ThenInclude(x => x.Author)
+                .Include(x => x.BookType)
+                .ThenInclude(x => x.TypeOfBook);
+            HomeVM model = new HomeVM
+            {
+                Books = books,
+                BookTypes = _db.BookType
+            };
+            return View(model);
         }
 
         public IActionResult Privacy()
