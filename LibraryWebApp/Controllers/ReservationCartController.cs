1c406fede52ce4e4f5900f7a21cd3f1f78849e35
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-20 16:54:10 +0100

    Add reservation cart display

---
+++ LibraryWebApp/Controllers/ReservationCartController.cs
@@ -0,0 +1,25 @@
+﻿using LibraryWebApp.Data;
+using LibraryWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryWebApp.Controllers
+{
+    public class ReservationCartController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ReservationCartController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCartList = new List<>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart))
+        }
+    }
+}
