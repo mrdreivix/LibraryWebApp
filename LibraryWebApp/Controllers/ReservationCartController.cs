75f93fca5c5ee0b3e02445a9da13717805b565cb
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-22 14:47:28 +0100

    Add functionality to add reservation

--- LibraryWebApp/Controllers/ReservationCartController.cs
+++ LibraryWebApp/Controllers/ReservationCartController.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.AspNetCore.Identity;
 
 namespace LibraryWebApp.Controllers
 { 
@@ -15,8 +17,11 @@ namespace LibraryWebApp.Controllers
     {
         private readonly AppDbContext _db;
 
-        public ReservationCartController(AppDbContext db)
+        private readonly UserManager<IdentityUser> userManager;
+
+        public ReservationCartController(AppDbContext db, UserManager<IdentityUser> userManager)
         {
+            this.userManager = userManager;
             _db = db;
         }
         public IActionResult Index()
@@ -44,7 +49,41 @@ namespace LibraryWebApp.Controllers
             HttpContext.Session.Set(WC.SessionCart, reservationCartList);
             return RedirectToAction(nameof(Index));
         }
-
+        public async Task<IActionResult> CreateReservation()
+        {
+            List<ReservationCartEntry> reservationCartList = new List<ReservationCartEntry>();
+            if (HttpContext.Session.Get<IEnumerable<ReservationCartEntry>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ReservationCartEntry>>(WC.SessionCart).Count() > 0)
+            {
+                reservationCartList = HttpContext.Session.Get<List<ReservationCartEntry>>(WC.SessionCart);
+            }
+            List<int> bookInCart = reservationCartList.Select(i => i.BookIdInCart).ToList();
+            var todayDate = DateTime.Now;
+            var userName = HttpContext.User.Identity.Name;
+            var user = await userManager.FindByNameAsync(userName);
+            Reservation reservation = new Reservation()
+            {
+                DateOfReservation = todayDate,
+                PlannedDateOfReturn = todayDate.AddMonths(+1),
+                IdClient= user.Id,
+            };
+            _db.Reservation.Add(reservation);
+            _db.SaveChanges();
+            var p = reservation.Id;
+            foreach(var i in bookInCart)
+            {
+                ReservationBook reservationBook = new ReservationBook()
+                {
+                    IdReservation = p,
+                    IdBook = i
+                };
+                _db.ReservationBook.Add(reservationBook);
+            }
+            _db.SaveChanges();
+            reservationCartList.Clear();
+            HttpContext.Session.Set(WC.SessionCart, reservationCartList);
+            return RedirectToAction(nameof(Index), "Home");
+        }
         
     }
 }
