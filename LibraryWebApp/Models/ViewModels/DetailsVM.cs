b579672e8df6a7b3ae8fdba7de7bc6a52a411037
Grzegorz Biel <admin@DESKTOP-9QI7CK6>
2022-01-22 10:21:34 +0100

    simple changes

--- LibraryWebApp/Models/ViewModels/DetailsVM.cs
+++ LibraryWebApp/Models/ViewModels/DetailsVM.cs
@@ -7,13 +7,7 @@ namespace LibraryWebApp.Models.ViewModels
 {
     public class DetailsVM
     {
-        public IEnumerable<Author> Authors { get; set; }
-        public IEnumerable<BookAuthor> BookAuthors { get; set; }
         public Book Book { get; set; }
-        public List<int> AuthorsId { get; set; }
-        public IEnumerable<BookBookType> BookTypes { get; set; }
-        public IEnumerable<BookType> Types { get; set; }
-        public List<int> BookTypesId { get; set; }
         public bool ExistsInCart { get; set; }
     }
 }
